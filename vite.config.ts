60fbd712c89fd4197c426a797e28d82886ed39f6
Gnanada <alavala.gnanada.542@gmail.com>
2025-11-16 16:33:09 +0530

    edited vite

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
     outDir: 'build',
   },
   server: {
-    port: 3000,
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
     open: true,
     proxy: {
       '/api': {
